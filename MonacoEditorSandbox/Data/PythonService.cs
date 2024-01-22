fec51bf1645a4939e8966adfe0f4623272179ec8
petrone.moolman <petrone.moolman@dyna-mo.com>
2024-01-22 14:57:31 +0200

    Run python code

---
+++ MonacoEditorSandbox/Data/PythonService.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Diagnostics;
+
+namespace MonacoEditorSandbox.Data;
+
+
+public class PythonService
+{
+    public async Task<string> RunScriptAsync(string script)
+    {
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = "python",
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        using var process = new Process();
+        process.StartInfo = processStartInfo;
+        process.Start();
+
+        await using (var sw = process.StandardInput)
+        {
+            await sw.WriteLineAsync(script);
+        }
+
+        var output = await process.StandardOutput.ReadToEndAsync();
+        var error = await process.StandardError.ReadToEndAsync();
+
+        return output + error;
+    }
+}
--- MonacoEditorSandbox/Program.cs
+++ MonacoEditorSandbox/Program.cs
@@ -5,6 +5,7 @@ using MonacoEditorSandbox.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<PythonService>();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
